29889bb8bb966a09f63f07ceff82182f180350d9
uczen <marcin.zawalisz@gmail.com>
2022-11-13 10:17:35 +0100

    2022.11.13

---
+++ src/com/desktop/Main.java
@@ -0,0 +1,13 @@
+package com.desktop;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame JFrame = new JFrame();
+        JFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JFrame.setTitle("Calculator");
+        JFrame.setVisible(true);
+        JFrame.setBounds(10, 20, 200, 300);
+    }
+}
---
+++ src/com/desktop/MainTwo.java
@@ -0,0 +1,13 @@
+package com.desktop;
+
+import javax.swing.*;
+
+public class MainTwo extends JFrame {
+    public static void main(String[] args) {
+        MainTwo jFrame = new MainTwo();
+        JFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JFrame.setTitle("Calculator");
+        JFrame.setVisible(true);
+        JFrame.setBounds(10, 20, 200, 300);
+
+}
