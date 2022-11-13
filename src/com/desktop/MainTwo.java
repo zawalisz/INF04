337e7c58b14e6fdcf3341f49b79bc4bb436adbce
uczen <marcin.zawalisz@gmail.com>
2022-11-13 10:43:56 +0100

    2022.11.12

--- src/com/desktop/MainTwo.java
+++ src/com/desktop/MainTwo.java
@@ -4,10 +4,11 @@ import javax.swing.*;
 
 public class MainTwo extends JFrame {
     public static void main(String[] args) {
-        MainTwo jFrame = new MainTwo();
+        MainTwo JFrame = new MainTwo();
         JFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         JFrame.setTitle("Calculator");
         JFrame.setVisible(true);
         JFrame.setBounds(10, 20, 200, 300);
 
+    }
 }
